565a1e58682597cdf1883093fd9e58ed77149b68
Karasca <roman_nl@live.nl>
2023-09-30 19:05:44 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import characterList from '../data/votedata.json';
+
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+  // ...
+} from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tvapp';
+  characters = characterList;
+  hideUpTo = 1000;
+  displayedColumns: string[] = ['ranking', 'ranking2022', 'ranking2021', 'name', 'points','nr1votes', 'comments', 'fanworks'];
+  
+  isGreater(rank:number){
+    return rank < this.hideUpTo;
+  }
+
+  setSelected(selected:number){
+    this.hideUpTo = selected;
+  }
+
+  ngOnInit() {
+    console.log('init');
+  }
 }
