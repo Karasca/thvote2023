5297f87150f4c72bfe9260a017849a333be7e3ef
Karasca <roman_nl@live.nl>
2023-09-30 19:51:23 +0200

    added a little warning

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,13 +20,19 @@ export class AppComponent implements OnInit{
   characters = characterList;
   hideUpTo = 1000;
   displayedColumns: string[] = ['ranking', 'ranking2022', 'ranking2021', 'name', 'points','nr1votes', 'comments', 'fanworks'];
-  
+  warned = false;
   isGreater(rank:number){
     return rank < this.hideUpTo;
   }
 
   setSelected(selected:number){
-    this.hideUpTo = selected;
+    if(this.hideUpTo > 50 && selected <= 20 && !this.warned){
+      this.warned = true;
+      alert(`You are about to reveal all the way up to #${selected} are you sure this is what you want???, if so click again. This warning will not appear again.`);
+    }else{
+      this.hideUpTo = selected;
+    }
+    
   }
 
   ngOnInit() {
